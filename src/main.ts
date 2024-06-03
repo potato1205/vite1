5dd2811847dc68f95ec110984b0caae774bcbbdb
potato1205 <915237695@qq.com>
2024-06-03 15:00:15 +0800

    add element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+
+// vue-router@4
 import router from './router/index.ts'
+
+// element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 createApp(App)
 .use(router)
+.use(ElementPlus)
 .mount('#app')
--- src/page/list-page.vue
+++ src/page/list-page.vue
@@ -1,12 +1,45 @@
 <template>
-  <div>
-    list-page
+  <div class="list-page" >
+    <el-transfer v-model="value" :data="data" />
   </div>
 </template>
 
 <script setup lang="ts">
-console.log('list-page');
+
+import { ref } from 'vue'
+
+interface Option {
+  key: number
+  label: string
+  disabled: boolean
+}
+
+const generateData = () => {
+  const data: Option[] = []
+  for (let i = 1; i <= 15; i++) {
+    data.push({
+      key: i,
+      label: `Option ${i}`,
+      disabled: i % 4 === 0,
+    })
+  }
+  return data
+}
+
+const data = generateData()
+console.log('data: ',data);
+
+const value = ref([])
 </script>
 
 <style scoped>
+/* @import "./list-page.scss"; */
+
+.list-page {
+  width: 100%;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
