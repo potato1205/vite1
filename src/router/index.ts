05205daebc6077c9f7196c7b973497081a22062e
potato1205 <915237695@qq.com>
2024-06-12 17:11:51 +0800

    add virtual scroll page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter,createWebHashHistory } from 'vue-router'
 import ListDemo from '../page/list-page.vue'
 import One from '../page/one.vue'
+import VirtualScroll from '../page/virtual-scroll/virtual-scroll-page.vue'
 
 const routes = [
-    { path: '/', redirect: '/list' },
+    { path: '/', redirect: '/virtual-scroll' },
     { path: '/list', component: ListDemo },
     { path: '/one', component: One },
+    { path: '/virtual-scroll', component: VirtualScroll}
   ]
   
   const router = createRouter({
