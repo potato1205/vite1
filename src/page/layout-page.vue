00445996cb48a74e9a74fa41d79546906a89d254
potato1205 <915237695@qq.com>
2024-06-18 17:37:33 +0800

    add layout-page

---
+++ src/page/layout-page.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="layout-page">
+    <div class="layout-flex">
+      <div class="color-box"></div>
+      <div class="color-box"></div>
+      <div class="color-box"></div>
+      <div class="color-box"></div>
+      <div class="color-box"></div>
+      <div class="color-box"></div>
+    </div>
+    <div class="layout-flex-two">
+      <div class="layout-flex-two-left"></div>
+      <div class="layout-flex-two-left"></div>
+      <div class="layout-flex-two-right"></div>
+    </div>
+    <div class="layout-grid">
+      <div class="layout-grid-box">1</div>
+      <div class="layout-grid-box">2</div>
+      <div class="layout-grid-box">3</div>
+      <div class="layout-grid-box">4</div>
+      <div class="layout-grid-box">5</div>
+      <div class="layout-grid-box">6</div>
+      <div class="layout-grid-box">7</div>
+      <div class="layout-grid-box">8</div>
+      <div class="layout-grid-box">9</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.layout-flex {
+  /* 设置了flex之后，内部元素默认为inline */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+}
+
+.color-box {
+  width: 100px;
+  height: 100px;
+  background-color: #f0f0f0;
+}
+
+.layout-flex-two {
+  display: flex;
+  height: 200px;
+
+  .layout-flex-two-left {
+    flex-shrink: 0;
+    width: 200px;
+    background-color: red;
+
+    &:nth-child(2) {
+      background-color: green
+    }
+  }
+
+  .layout-flex-two-right {
+    flex-grow: 1;
+    background-color: yellow;
+  }
+}
+
+.color-box:nth-child(odd) {
+  background-color: blue;
+}
+
+.color-box:nth-child(even) {
+  background-color: yellow;
+}
+
+// 网格布局
+.layout-grid {
+  display: grid;
+  justify-content: stretch ;
+  grid-template-columns: 100px auto auto;
+  grid-template-rows: 100px 100px 100px;
+  // height: 800px;
+
+  .layout-grid-box {}
+
+  .layout-grid-box:nth-child(odd) {
+    background-color: blue;
+  }
+
+  .layout-grid-box:nth-child(even) {
+    background-color: yellow;
+  }
+
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,14 @@ import { createRouter,createWebHashHistory } from 'vue-router'
 import ListDemo from '../page/list-page.vue'
 import One from '../page/one.vue'
 import VirtualScroll from '../page/virtual-scroll/virtual-scroll-page.vue'
+import Layout from '../page/layout-page.vue'
 
 const routes = [
-    { path: '/', redirect: '/virtual-scroll' },
+    { path: '/', redirect: '/layout' },
     { path: '/list', component: ListDemo },
     { path: '/one', component: One },
-    { path: '/virtual-scroll', component: VirtualScroll}
+    { path: '/virtual-scroll', component: VirtualScroll},
+    { path: '/layout', component: Layout}
   ]
   
   const router = createRouter({
